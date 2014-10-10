66d5e3ccda0529dcdf39dff4745f56e4a3b1605d
mooophy <MoophyAlan@gmail.com>
2014-10-10 18:48:56 +1300

    add ctor with unsigned int 	modified:   big/biguint.cpp

--- big/biguint.cpp
+++ big/biguint.cpp
@@ -1,28 +1,80 @@
+//! @note   several c++11 features are being used.
+//!         to compile :
+//!         g++ -std=c++11 -Wall main.cpp
+
 #include <iostream>
 #include <string>
+#include <stdexcept>
+#include <list>
+#include <assert.h>
 #include "biguint.h"
 
-char gen_digit(char number)
+//! using 'h' (head) as the sentinel.
+inline char
+gen_digit(char number)
 {
     return number + 48;
 }
 
+inline unsigned
+size(const char* p, char end)
+{
+    assert(p);
+
+    unsigned ret = 0;
+    for(; *p != end; ++p)
+        ++ret;
+
+    return ret;
+}
+
+inline unsigned
+how_many_digit(unsigned int num)
+{
+    unsigned ret = 1;
+    for(; num > 9; num /= 10)
+        ++ret;
+
+    return ret;
+}
+
 //! default Ctor
-bigUInt::bigUInt():
+inline bigUInt::bigUInt():
     p(new char[2])
 {
     p[0] = gen_digit(0);
-    p[1] = '\0';
+    p[1] = 'h';
+}
+
+/**
+ * @brief bigUInt::bigUInt
+ * @param unsigned int
+ */
+bigUInt::bigUInt(unsigned int n)
+{
+    std::string buff = std::to_string(n);
+
+    //! allocate p and set tail
+    p = new char[buff.size() + 1];
+    assert(p);
+    auto curr = p + buff.size();
+    *curr-- = 'h';
+
+    //! fill the rest
+    for(auto digit : buff)
+        *curr-- = digit;
 }
 
 //! Dtor
-bigUInt::~bigUInt()
+inline bigUInt::~bigUInt()
 {
     delete[] p;
 }
 
-std::ostream & operator<<(std::ostream &out, const bigUInt &x)
+inline std::ostream&
+operator<<(std::ostream &out, const bigUInt &x)
 {
-    out << x.get_p();
+    for(unsigned curr = size(x.get_p(), 'h'); curr != 0; --curr)
+        std::cout << x.get_p()[curr - 1];
     return out;
 }
--- big/main.cpp
+++ big/main.cpp
@@ -2,11 +2,14 @@
 #include <string>
 #include "biguint.h"
 
+
+
 int main()
 {
-    bigUInt d0;
+    bigUInt d0(4208800);
 
     std::cout << d0 << std::endl;
 
+    std::cout << "\nexit normally\n";
     return 0;
 }
