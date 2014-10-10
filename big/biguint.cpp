d393331fc9530e1b16023f2e792fdc6352e9edb5
mooophy <MoophyAlan@gmail.com>
2014-10-10 21:00:58 +1300

    refactored, using std instead of manual handling 	modified:   big/biguint.cpp 	modified:   big/main.cpp

--- big/biguint.cpp
+++ big/biguint.cpp
@@ -2,3 +2,55 @@
 //!         to compile :
 //!         g++ -std=c++11 -Wall main.cpp
 
+#include <string>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <algorithm>
+#include "biguint.h"
+
+inline char*
+build_str(const std::string& buff)
+{
+    auto dest = new char[buff.size() + 1];
+    std::copy(buff.begin(), buff.end(), dest);
+    dest[buff.size()] = '\0';
+
+    return dest;
+}
+
+//! default Ctor
+bigUInt::bigUInt():
+    p{new char[2]}
+{
+    p[0] = '0';
+    p[1] = '\0';
+}
+
+//! Dtor
+bigUInt::~bigUInt()
+{
+    delete[] p;
+}
+
+bigUInt::bigUInt(unsigned int n):
+    p{build_str(std::to_string(n))}
+{}
+
+bigUInt::bigUInt(const char *s):
+    p{build_str(s)}
+{}
+
+bigUInt::bigUInt(const bigUInt &x):
+    p{build_str(x.get_p())}
+{}
+
+void bigUInt::print()
+{
+    std::cout << p;
+}
+
+std::ostream & operator<<(std::ostream &out, const bigUInt &x)
+{
+    return out << x.get_p();
+}
--- big/main.cpp
+++ big/main.cpp
@@ -6,7 +6,14 @@
 
 int main()
 {
+    bigUInt number0("42");
+    bigUInt number1{number0};
 
+    number0.print();
+    std::cout << "\n";
+    number1.print();
+    std::cout << "\n";
+    std::cout << number0;
 
     std::cout << "\nexit normally\n";
     return 0;
