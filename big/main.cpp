8d7e49e80edf11fe7479e524e67b9e1805206f53
mooophy <MoophyAlan@gmail.com>
2014-10-10 20:23:05 +1300

    modified:   big/biguint.cpp 	modified:   big/main.cpp

--- big/main.cpp
+++ big/main.cpp
@@ -6,14 +6,7 @@
 
 int main()
 {
-    bigUInt d0("4477886678990000998622223344");
-    bigUInt d1(d0);
 
-    std::cout << d0 << std::endl << d1 << std::endl;
-
-    d0.print();
-    std::cout << "\n";
-    d1.print();
 
     std::cout << "\nexit normally\n";
     return 0;
